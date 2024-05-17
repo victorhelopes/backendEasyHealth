6fd4dc21c82f637e81bb0c142bc21a5d72fbedbb
victorhelopes <victorhelopes@gmail.com>
2024-05-17 20:27:48 -0300

    add: .env config file

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -3,6 +3,8 @@ import routes from './routes/index.routes';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import '../config/env';
+
 const app = express();
 
 app.use(cors())
@@ -10,8 +12,8 @@ app.use(cors())
 app.use(express.json());
 app.use(routes);
 
-app.listen(4000, () => {
-    mongoose.connect("mongodb+srv://FeiraEJ:FeiraEJ@cluster0.wr1cu.mongodb.net/?retryWrites=true&w=majority");
+app.listen(process.env.PORT, () => {
+    mongoose.connect(process.env.DATA_BASE_URL  as string);
   });
 
 export default app;
